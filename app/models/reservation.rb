58d4ec3c84b740f15be4cf334bfbfd11035a1363
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-14 13:07:12 -0400

    Complete reservation deliverables

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,40 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
+  validate :not_host
+  validate :is_available?
+  validate :checkin_after_checkout
+
+  def not_host
+    if listing.host_id == guest_id
+      errors.add(:guest_id, "Cannot book own listing")
+    end
+  end
+
+  def is_available?
+    Reservation.where(listing_id: listing.id).where.not(id: id).each do |reso|
+      dates = reso.checkin..reso.checkout
+      if dates === checkin || dates === checkout
+        errors.add(:guest_id, "Currently booked")
+      end
+    end
+  end
+
+  def checkin_after_checkout
+    if checkin && checkout && checkout <= checkin
+      errors.add(:guest_id, "Gotta stay before you can leave")
+    end
+  end
+
+  def duration
+    days = checkout - checkin
+    days.to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
 end
