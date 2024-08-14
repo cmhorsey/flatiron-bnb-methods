77497d4bb45d7deac254adb15a3352cdb6ee2e18
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-14 15:14:20 -0400

    Complete User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,36 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+  # go through TRIPS
+  def guests
+    self.reservations.map do |reso|
+      reso.guest
+    end
+  end
+
+  def hosts
+    self.trips.map do |trip|
+      trip.listing.host
+    end
+  end
+
+  # def host_reviews
+  #   self.trips.map do |trip|
+  #     trip.review
+  #   end
+  # end
+
+  # def host_reviews
+  #   self.guests.map |guest| do
+  #     guest.review
+  #   end
+  # end
+
+  def host_reviews
+    self.reservations.map do |reso|
+      reso.review
+    end
+  end
+
 end
