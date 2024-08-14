ecc0b58ba9be68a5a11c854ed11a0a6b8bbabcf3
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-14 15:35:52 -0400

    Complete City methods

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,21 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
-  has_many :listings, :through => :neighborhoods
+  has_many :listings, through: :neighborhoods
 
+  def city_openings(start_date, end_date)
+    listings.where.not(id: Reservation.select(:listing_id)
+      .where('checkin < ? AND checkout > ?', end_date, start_date))
+  end
+
+  def self.highest_ratio_res_to_listings
+    City.all.max_by do |city|
+      listings_count = city.listings.count
+      reservations_count = city.listings.joins(:reservations).count
+      reservations_count / listings_count
+    end
+  end
+
+  def self.most_res
+    City.all.max_by { |city| city.listings.joins(:reservations).count }
+  end
 end
-
