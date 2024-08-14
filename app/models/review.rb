679ccdf8facdbb3f962d2a0e29ad1a01a3a92569
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-14 15:32:47 -0400

    Complete Review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+
+  validate :existing_elapsed_reservation
+
+  def existing_elapsed_reservation
+    if reservation.nil? || reservation.checkout >= Date.today || reservation.status != 'accepted'
+      errors.add(:reservation, "try again")
+    end
+  end
+
+  # use or
+  # if there is not a reso or not accepted
 end
